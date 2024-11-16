1aaeb6c823ad208cc009271ae696e4070691c107
Haris Mughal <harismirza3456@gmail.com>
2024-11-16 22:24:07 +0500

    update logs

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 
 function logReqRes(fileName) {
     return (req, res, next) => {
+        if (req.url == "/favicon.ico") return res.end();
+
         fs.appendFile(
             fileName,
             `\n${Date.now()}: ${req.method} - ${req.url}`,
