76c06a4d7d27ecaf514dab2972aadd19f8a7d6a8
Haris Mughal <harismirza3456@gmail.com>
2024-11-18 20:26:54 +0500

    managed routes for authentication

---
+++ controllers/user.js
@@ -0,0 +1,40 @@
+const { v4: uuidv4 } = require("uuid");
+const User = require("../models/user");
+const { setUser } = require("../services/auth");
+
+async function userSignUp(req, res) {
+    const { name, email, password } = req.body;
+
+    await User.create({
+        name,
+        email,
+        password,
+    });
+
+    return res.redirect("/");
+}
+
+async function userLogin(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+        email,
+        password,
+    });
+
+    if (!user)
+        return res.render("login", {
+            error: "Invalid credentials",
+        });
+
+    const sessionId = uuidv4();
+    setUser(sessionId, user);
+
+    res.cookie("uid", sessionId);
+
+    return res.redirect("/");
+}
+
+module.exports = {
+    userSignUp,
+    userLogin,
+};
