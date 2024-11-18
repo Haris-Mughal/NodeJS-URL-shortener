6e54ea57c751bfc5128f3308605890b52c4ea827
Haris Mughal <harismirza3456@gmail.com>
2024-11-18 20:31:57 +0500

    rendered

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const { connectDB } = require("./connectDB");
-const URL = require("./models/url");
-const urlRouter = require("./routes/url");
-const staticRoute = require("./routes/staticRouter");
 const { logReqRes } = require("./middlewares");
+const { showUrlsOfUsers, checkAuth } = require("./middlewares/auth");
+
 const path = require("path");
+const cookieParser = require("cookie-parser");
+
+const urlRouter = require("./routes/url");
+const userRouter = require("./routes/user");
+const staticRoute = require("./routes/staticRouter");
 
 const app = express();
 const PORT = 8000;
@@ -13,23 +17,16 @@ connectDB(`mongodb://localhost:27017/short-url`);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(logReqRes("log.txt"));
 
-app.use("/url", urlRouter);
-app.use("/", staticRoute);
+app.use("/url", showUrlsOfUsers, urlRouter);
+app.use("/user", userRouter);
+app.use("/", checkAuth, staticRoute);
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
-
-// app.use("/", async (req, res) => {
-//     const allURLs = await URL.find({});
-
-//     return res.render("home");
-//     // return res.render("home", {
-//     //     urls: allURLs,
-//     // });
-// });
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
